d719bf6695acfe461c79352aefa04d3c4248c874
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-04 10:59:34 +0000

    Format

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-
 def read_assignment(line):
     parts = line.split(',')
     left = parts[0].split('-')
-    right = parts[1].split('-')    
+    right = parts[1].split('-')
     return Assignment(int(left[0]), int(left[1]), int(right[0]), int(right[1]))
 
+
 def left_inside_right(one_left, one_right, two_left, two_right):
-    return one_left >= two_left and one_left <= two_right and one_right <= two_right and one_right >= two_left
+    return one_left >= two_left and one_left <= two_right and \
+    one_right <= two_right and one_right >= two_left
+
 
 class Assignment:
+
     def __init__(self, first_left, first_right, second_left, second_right):
         self.first_left = first_left
         self.first_right = first_right
         self.second_left = second_left
         self.second_right = second_right
-    
+
     def inside(self):
         return left_inside_right(self.first_left, self.first_right, self.second_left, self.second_right) or \
         left_inside_right(self.second_left, self.second_right, self.first_left, self.first_right)
@@ -25,8 +28,9 @@ class Assignment:
     def overlap(self):
         return not self.disjoint()
 
+
 assignments = []
-        
+
 with open('Input.txt') as f:
     for line in f:
         assignments.append(read_assignment(line))
